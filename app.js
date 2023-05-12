0793f54190e72f0e4cdb8c8e2c7bedacc4e42f8e
Leon8M <leonmunene254@gmail.com>
2023-05-12 23:16:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let box = document.querySelector('.box')
+let logo = document.querySelector('.logo')
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+        setTimeout(()=>{
+            box.style.animation = 'splash 1s ease forwards 2.2s';
+        },400)
+   })
+      
+})  
