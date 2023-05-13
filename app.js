e0323e554fc0e69d0f574d460787cc786f116740
Leon8M <leonmunene254@gmail.com>
2023-05-13 09:16:06 +0300

    Update

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ window.addEventListener('DOMContentLoaded', ()=>{
    })
       
 })  
+var scrollLinks = document.querySelectorAll(".scroll-link");
+
+for (var i = 0; i < scrollLinks.length; i++) {
+  scrollLinks[i].addEventListener("click", function(event) {
+    event.preventDefault(); // prevent the default behavior of the link
+    var targetId = this.getAttribute("href");
+    var targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({ behavior: "smooth" }); // scroll to the target element
+  });
+}
