d6f6c1700bd579794e333d2fea8702712a3676f9
Leon8M <leonmunene254@gmail.com>
2023-05-30 16:12:16 +0300

    All

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-let box = document.querySelector('.box')
-let logo = document.querySelector('.logo')
+let box = document.querySelector('.box');
+let logo = document.querySelector('.logo');
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    setTimeout(()=>{
-        setTimeout(()=>{
-            box.style.animation = 'splash 1s ease forwards 2.2s';
-        },400)
-   })
-      
-})  
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    box.style.animation = 'splash 1s ease forwards 2.2s';
+  }, 400);
+});
+
 var scrollLinks = document.querySelectorAll(".scroll-link");
 
 for (var i = 0; i < scrollLinks.length; i++) {
   scrollLinks[i].addEventListener("click", function(event) {
-    event.preventDefault(); // prevent the default behavior of the link
+    event.preventDefault();
     var targetId = this.getAttribute("href");
     var targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: "smooth" }); // scroll to the target element
+    targetElement.scrollIntoView({ behavior: "smooth" });
   });
 }
+
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.drop');
 const dropDown = document.querySelector('.dropdown');
@@ -26,13 +25,13 @@ var arrow = './images/arrow.png';
 var cancel = './images/cancel.png';
 var isAlternate = false;
 
-toggleBtn.addEventListener("click", function (){
+toggleBtn.addEventListener("click", function() {
   dropDown.classList.toggle('open');
   const isOpen = dropDown.classList.contains('open');
-  if (isAlternate){
+  if (isAlternate) {
     toggleBtnIcon.src = arrow;
     isAlternate = false;
-  } else{
+  } else {
     toggleBtnIcon.src = cancel;
     isAlternate = true;
   }
